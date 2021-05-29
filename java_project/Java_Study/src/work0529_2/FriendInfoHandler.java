909c4b474daeea37612858a6d43139166df19bfb
SeraWi <wsr2357@naver.com>
2021-05-29 14:58:08 +0900

    .

---
+++ java_project/Java_Study/src/work0529_2/FriendInfoHandler.java
@@ -0,0 +1,5 @@
+package work0529_2;
+
+public class FriendInfoHandler {
+
+}
---
+++ java_project/Java_Study/src/work0529_2/UnivFriend.java
@@ -0,0 +1,24 @@
+package work0529_2;
+
+//대학친구는 전공정보를 가진다.
+public class UnivFriend extends Friend {
+	
+	String major;
+	public UnivFriend(String name, String phoneNumber, String address) {
+		super(name, phoneNumber, address);
+		this.major = major;
+	}
+	@Override
+	public void showData() {
+		super.showData();
+		System.out.println("전   공: "+major);
+	}
+	@Override
+	public void showBasicInfo() {
+		System.out.println("이  름: "+ name);
+		System.out.println("전  공: "+ major);
+	}
+	
+
+	
+}
