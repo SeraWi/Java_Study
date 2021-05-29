49712e5f1e3114f772f338ffd51e67811b7668dd
SeraWi <wsr2357@naver.com>
2021-05-29 14:47:48 +0900

    Create Friend.java

---
+++ java_project/Java_Study/src/work0529_2/Friend.java
@@ -0,0 +1,23 @@
+package work0529_2;
+
+// 상속을 목적으로 하는 클래스
+public class Friend {
+	String name;
+	String phoneNumber;
+	String address;
+	
+	public Friend(String name, String phoneNumber, String address) {
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+	}
+	public void showData() {
+		System.out.println("이   름: "+name);
+		System.out.println("전화번호: "+phoneNumber);
+		System.out.println("주   소: "+address);
+	}
+	public void showBasicInfo() {
+		
+	}
+	
+}
