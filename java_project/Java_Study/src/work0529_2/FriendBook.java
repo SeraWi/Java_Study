c7c8d747611d369a3e08bf2d2022b2c73d8c57a0
SeraWi <wsr2357@naver.com>
2021-05-29 15:47:45 +0900

    new

--- java_project/Java_Study/src/work0529_2/FriendBook.java
+++ java_project/Java_Study/src/work0529_2/FriendBook.java
@@ -6,33 +6,34 @@ public class FriendBook {
 
 	public static void main(String[] args) {
 		FriendInfoHandler handler = new FriendInfoHandler(10);
-		
+
 		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("=====메뉴 선택======");
-		System.out.println("1. 고교 친구 정보 입력");
-		System.out.println("2. 대학 친구 정보 입력");
-		System.out.println("3. 전체 정보 출력");
-		System.out.println("4. 기본 정보 출력");
-		System.out.println("5. 프로그램 종료");
-		System.out.println("==================");
-		
-		System.out.println("메뉴를 선택하세요 > ");
-		int choice = Integer.parseInt(scanner.nextLine());
-		
-		switch(choice) {
-		case 1: case 2:
-			handler.addFriend(choice);
-			break;
-		case 3:
-			handler.showAllData();
-			break;
-		case 4:
-			handler.showAllBaiscData();
-			break;
-		case 5: 
-			System.out.println("프로그램을 종료합니다.");
-			return;
+		while(true) {
+			System.out.println("=====메뉴 선택======");
+			System.out.println("1. 고교 친구 정보 입력");
+			System.out.println("2. 대학 친구 정보 입력");
+			System.out.println("3. 전체 정보 출력");
+			System.out.println("4. 기본 정보 출력");
+			System.out.println("5. 프로그램 종료");
+			System.out.println("==================");
+
+			System.out.println("메뉴를 선택하세요 > ");
+			int choice = Integer.parseInt(scanner.nextLine());
+
+			switch(choice) {
+			case 1: case 2:
+				handler.addFriend(choice);
+				break;
+			case 3:
+				handler.showAllData();
+				break;
+			case 4:
+				handler.showAllBaiscData();
+				break;
+			case 5: 
+				System.out.println("프로그램을 종료합니다.");
+				return;
+			}
 		}
 	}
 }
---
+++ java_project/Java_Study/src/work0529_3/Student.java
@@ -0,0 +1,22 @@
+package work0529_3;
+
+public class Student {
+	// 학생 성적, 총합, 평균, 정보보기
+	String name;
+	int kor;
+	int eng;
+	int math;
+	
+	Student(String name, int kor, int eng, int math){
+		this.name = name;
+		this.kor =kor;
+		this.eng = eng;
+		this.math = math;
+	}
+	public void totalScore() {
+		System.out.println(name+"의 총점은 "+ kor+eng+math+" 점 입니다.");
+	}
+	public void avgScore() {
+		System.out.println(name+"의 평균은 "+(float)((kor+eng+math)/3)+" 점 입니다.");
+	}
+}
