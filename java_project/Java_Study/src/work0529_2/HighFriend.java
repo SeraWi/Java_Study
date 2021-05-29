eea423241daf945f66ad0aaa8077bfb62e32431d
SeraWi <wsr2357@naver.com>
2021-05-29 14:51:56 +0900

    Create HighFriend.java

---
+++ java_project/Java_Study/src/work0529_2/HighFriend.java
@@ -0,0 +1,21 @@
+package work0529_2;
+ 
+//고교 친구는 직장 정보를 추가적으로 가진다.
+public class HighFriend extends Friend{
+	String work;
+	public HighFriend(String name, String phoneNumber, String address) {
+		super(name, phoneNumber, address);
+		this.work = work;
+	}
+	@Override
+	public void showData() {
+		super.showData();
+		System.out.println("직   업: " + work);
+	}
+	@Override
+	public void showBasicInfo() {
+		System.out.println("이   름: "+ name);
+		System.out.println("직   업: "+ work);
+	}
+	
+}
