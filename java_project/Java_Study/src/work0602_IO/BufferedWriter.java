a42825a0ef4bd785a9c0d66cee74cd7c9736a3c2
SeraWi <wsr2357@naver.com>
2021-06-05 21:22:42 +0900

    .

---
+++ java_project/Java_Study/src/work0602_IO/BufferedWriter.java
@@ -0,0 +1,10 @@
+package work0602_IO;
+
+public class BufferedWriter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java_project/Java_Study/src/work0605/ThreadSyncTest.java
@@ -0,0 +1,10 @@
+package work0605;
+
+public class ThreadSyncTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
