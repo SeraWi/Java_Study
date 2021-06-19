53f6059837e1803a391385dc51ea6a5cf0138b0f
SeraWi <wsr2357@naver.com>
2021-06-19 22:59:26 +0900

    Update jdbcTest.sql

--- SQL/work0619/jdbcTest.sql
+++ SQL/work0619/jdbcTest.sql
@@ -2,6 +2,24 @@
 --JDBC test
 
 
-select * 
-from dept 
-order by name;
+select *from dept order by dname;
+
+select * from dept where deptno = 10;
+
+select e.empno, e.ename, e.job, e.sal, d.dname,d.loc
+from emp e, dept d
+where e.deptno = d.deptno;
+
+create table dept01
+as select * from dept where 1= 0;
+select * from dept01;
+-- dept01테이블에 deptno에 입력할 일련 번호 ->sequence
+create sequence dept01_deptno_seq
+start with 10
+increment by 10;
+
+insert into dept01 values(dept01_deptno_seq.nextval, dev, seoul);
+
+
+select * from dept01;
+select * from dept;
