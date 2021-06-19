8953bae9e674b813ae5a43b6dcd99c90866f5b14
SeraWi <wsr2357@naver.com>
2021-06-19 19:21:13 +0900

    Create Dept.java

---
+++ java_project/Java_Study2/src/dept/Dept.java
@@ -0,0 +1,50 @@
+package dept;
+
+public class Dept {
+	//사용자 입력 Data를 dept에 넣기
+	
+	private int deptno; // 부서번호
+	private String dname; //부서 이름
+	private String loc; //부서 위치
+	
+	//생성자
+	public Dept(int deptno, String dname, String loc) {
+		this.deptno = deptno;
+		this.dname = dname;
+		this.loc = loc;
+	}
+
+	//Getter, Setter
+	
+	public int getDeptno() {
+		return deptno;
+	}
+
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+
+	public String getDname() {
+		return dname;
+	}
+
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+
+	public String getLoc() {
+		return loc;
+	}
+
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+
+	@Override
+	public String toString() {
+		return "Dept [deptno=" + deptno + ", dname=" + dname + ", loc=" + loc + "]";
+	}
+	
+	
+
+}
