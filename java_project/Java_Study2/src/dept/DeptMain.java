246e5015d4ab953046223020b00765bcb1664cb5
SeraWi <wsr2357@naver.com>
2021-06-19 23:20:06 +0900

    Update DeptMain.java

--- java_project/Java_Study2/src/dept/DeptMain.java
+++ java_project/Java_Study2/src/dept/DeptMain.java
@@ -54,7 +54,38 @@ public class DeptMain {
 				System.out.println("입력 실패!");
 			}
 			
-		
+//---------------------------------------------------------------------------
+			// UPDATE : UPDATE
+			System.out.println("부서 수정을 시작합니다.");
+			System.out.println("수정하고자 하는 부서번호 새로운 부서이름 새로운 부서위치를 입력해주세요.");
+			System.out.println("예) 50 dev seoul");
+			String deptData2 = sc.nextLine();
+			
+			//공백을 기준으로 잘라서 배열로 넣기
+			String[] deptDatas2 = deptData2.split(" ");
+			Dept dept2 = new Dept(Integer.parseInt(deptDatas2[0]),deptDatas2[1],deptDatas2[2]);
+			
+			int result2 = dao.editDept(conn, dept2);
+			if(result2 > 0) {
+				System.out.println("수정 성공");
+			}else {
+				System.out.println("수정 실패!");
+			}
+			
+//---------------------------------------------------------------------------
+			// DELETE : DELETE
+			
+			System.out.println("부서정보를 삭제합니다.");
+			System.out.println("삭제할 부서번호를 입력해주세요.");
+			String deptno = sc.nextLine();
+			
+			int result3 = dao.deleteDept(conn, Integer.parseInt(deptno));
+			
+			if(result3 > 0) {
+				System.out.println("삭제 완료");
+			}else {
+				System.out.println("삭제 실패 : 해당 번호의 부서가 존재하지 않습니다.  ");
+			}
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
