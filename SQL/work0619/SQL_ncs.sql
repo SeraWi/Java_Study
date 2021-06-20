05b6c5e816a23b71bd148915630c919846ffa122
SeraWi <wsr2357@naver.com>
2021-06-20 17:27:41 +0900

    Update SQL_ncs.sql

--- SQL/work0619/SQL_ncs.sql
+++ SQL/work0619/SQL_ncs.sql
@@ -64,3 +64,36 @@ select * from emp;
 rollback;
 
 
+--------------------------------------------------------------------------------------
+--1. 대리키 IDX, 회원 아이디, 이름, 비밀번호, 사진이름, 가입일 을 저장할 수 있는
+--    이름이 MEMBER인 테이블을 생성하세요.
+--    회원 아이디에는 기본 키 제약설정을 하고, 이름과 비밀번호에는 not null,
+--    가입 일은 기본 값으로 sysdate가 입력되도록 하시오.
+
+create table member(
+    idx number(4) not null unique,
+    memberId varchar2(20) constraint memeber_Id_pk primary key,
+    name varchar2(20) not null,
+    pw varchar2(20) not null,
+    imageName varchar2(20),
+    regDate date default sysdate
+);
+
+
+--  2. MEMBER 테이블에서 이름 컬럼으로 인덱스 객체를 생성하시오.
+
+create index  index_member_name
+on member(name);
+
+-- 3. MEMBER 테이블에서 회원 아이디, 이름, 사진 정보만을 출력하는 view 객체를 생성하시오.
+--view의 이름은 member_view 로 생성하시오
+create or replace view member_view
+as
+select memberId, name, imageName
+from member;
+
+select * from member_view;
+
+
+
+
