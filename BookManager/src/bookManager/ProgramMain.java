17544a9bd7e97fb1131c58dab5d8f7172f390a46
SeraWi <wsr2357@naver.com>
2021-06-21 12:30:58 +0900

    Update ProgramMain.java

--- BookManager/src/bookManager/ProgramMain.java
+++ BookManager/src/bookManager/ProgramMain.java
@@ -4,7 +4,11 @@ public class ProgramMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		BookManager manager = new BookManager(BookDao.getInstance());
+		
+		manager.bookList();
+		
 	}
 
 }
