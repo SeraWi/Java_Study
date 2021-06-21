e06eb26ddc1fda4cdc4b1523f0558b9c2c94d264
SeraWi <wsr2357@naver.com>
2021-06-21 12:30:51 +0900

    Update Book.java

--- BookManager/src/bookManager/Book.java
+++ BookManager/src/bookManager/Book.java
@@ -1,5 +1,52 @@
 package bookManager;
 
 public class Book {
+	private int bookid; //pk
+	private String bookname;
+	private String publisher;
+	private int price;
+	
+		
+	//생성자
+	public Book(int bookid, String bookname, String publisher, int price) {
+		super();
+		this.bookid = bookid;
+		this.bookname = bookname;
+		this.publisher = publisher;
+		this.price = price;
+	}
+	
+	//Getter, Setter
+	public int getBookid() {
+		return bookid;
+	}
+	public void setBookid(int bookid) {
+		this.bookid = bookid;
+	}
+	public String getBookname() {
+		return bookname;
+	}
+	public void setBookname(String bookname) {
+		this.bookname = bookname;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
 
+	@Override
+	public String toString() {
+		return "Book [bookid=" + bookid + ", bookname=" + bookname + ", publisher=" + publisher + ", price=" + price
+				+ "]";
+	}
+	
+	
 }
