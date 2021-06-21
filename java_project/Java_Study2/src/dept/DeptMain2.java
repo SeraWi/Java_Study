ded63e0d26613c9503838c54d3633539b99b12a8
SeraWi <wsr2357@naver.com>
2021-06-21 11:33:15 +0900

    Update DeptMain2.java

--- java_project/Java_Study2/src/dept/DeptMain2.java
+++ java_project/Java_Study2/src/dept/DeptMain2.java
@@ -7,41 +7,43 @@ public class DeptMain2 {
 	public static void main(String[] args) {
 		DeptManager manager = new DeptManager(DeptDao.getInstance());
 		Scanner sc = new Scanner(System.in);
-		
-		
-	
+
+
+
 		try {
 			// 1. 드라이버 로드
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			
-			
-			System.out.println("부서 관리 프로그램");
-			System.out.println("-----------------------------------------");
-			System.out.println("1. 부서 리스트");
-			System.out.println("2. 부서 정보 입력");
-			System.out.println("3. 부서 정보 수정");
-			System.out.println("4. 부서 정보 삭제");
-			System.out.println("5. 프로그램 종료");
-			System.out.println("-----------------------------------------");
-			System.out.println("원하시는 기능의 번호를 입력해주세요.");
-			int num = Integer.parseInt(sc.nextLine());
-			
-			switch(num) {
-			case 1:
-				manager.deptList();
-				break;
-			case 2:
-				manager.inputData();
-				break;
-			case 3:
-				break;
-			case 4:
-				break;
-			case 5:
-				break;
-			}
-			
-			
+
+			while(true) {
+				System.out.println("부서 관리 프로그램");
+				System.out.println("-----------------------------------------");
+				System.out.println("1. 부서 리스트");
+				System.out.println("2. 부서 정보 입력");
+				System.out.println("3. 부서 정보 수정");
+				System.out.println("4. 부서 정보 삭제");
+				System.out.println("5. 프로그램 종료");
+				System.out.println("-----------------------------------------");
+				System.out.println("원하시는 기능의 번호를 입력해주세요.");
+				int num = Integer.parseInt(sc.nextLine());
+
+				switch(num) {
+				case 1:
+					manager.deptList();
+					break;
+				case 2:
+					manager.inputData();
+					break;
+				case 3:
+					manager.editDept();
+					break;
+				case 4:
+					manager.delDept();
+					break;
+				case 5:
+					break;
+				}
+			}	
+
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
